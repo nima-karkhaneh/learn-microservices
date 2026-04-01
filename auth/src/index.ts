3c8c1cad2c396bf25fa5d0f2f6b9a8e9247af132
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2026-04-01 20:07:45 +1100

    Rewrite dev script for better hot-reloads / switch to Docker Desktop from GKE

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express"
 import { currentUserRouter } from "./routes/current-user.js";
+import { signinRouter } from "./routes/signin.js";
+import { signoutRouter } from "./routes/signout.js";
+import { signupRouter } from "./routes/signup.js";
 
 
 const app = express()
@@ -7,8 +10,11 @@ app.use(express.json())
 
 
 app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!!')
+    console.log('Test 5!!!!')
 })
